e637c4e231efc27890274e5c2727da068f956cd1
AtomicTenebris <atomictenebris@pm.me>
2026-06-18 12:58:22 +0530

    Placeholder configs

---
+++ configs/nvim/init.lua
@@ -0,0 +1,1 @@
+-- Placeholder will configure later
--- configs/powershell/user_profile.ps1
+++ configs/powershell/user_profile.ps1
@@ -1,2 +1,6 @@
 Set-Alias -Name code -Value code-insiders
 Set-Item -Path Function:chr -Value {code -r .}
+
+function Install-Dotfiles(){
+    irm "https://raw.githubusercontent.com/AtomicTenebris/dotfiles/main/bootstrap.ps1" | iex
+}
